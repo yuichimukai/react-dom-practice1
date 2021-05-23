b63a181a36f9eda88b87c8cf97cc573d56c55612
Y.M <66901428+yuichimukai@users.noreply.github.com>
2021-05-23 15:06:48 +0900

    props and state function

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ColorfulMessage from "./components/ColorfulMessage";
+const App = () => {
+  const onClickButton = () => alert();
+  return (
+    <>
+      <h1 style={{ color: "red" }}>こんにちは</h1>
+      <ColorfulMessage color="blue">おげんきですか？</ColorfulMessage>
+      <ColorfulMessage color="pink">元気です</ColorfulMessage>
+      <button onClick={onClickButton}>ボタン</button>
+    </>
+  );
+};
+
+export default App;
