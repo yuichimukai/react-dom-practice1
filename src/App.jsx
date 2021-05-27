717432c376a6c5b6cb427b8bb1cce2ffc2ca2774
Y.M <66901428+yuichimukai@users.noreply.github.com>
2021-05-27 16:41:25 +0900

    userState and useEffect practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
-import React from "react";
-import ColorfulMessage from "./components/ColorfulMessage";
+/* eslint react-hooks/exhaustive-deps: off */
+import React, { useEffect, useState } from "react";
+import { ColorfulMessage } from "./components/ColorfulMessage";
 const App = () => {
-  const onClickButton = () => alert();
+  console.log("さいしょ");
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  /* 第2引数が空の配列の場合は一回のみ実行できる、処理の分離もできる */
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+  }, [num]);
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは</h1>
       <ColorfulMessage color="blue">おげんきですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気です</ColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
+      <p>{num}</p>
+      {faceShowFlag && <p>＼(^o^)／</p>}
     </>
   );
 };
 
 export default App;
+
+// 再レンダリングしたとき
+// prpsの中身が変わった時、stateがかわったとき、親のコンポーネントが更新された時についじゅうして子のコンポーネントも再レンダリングされる
